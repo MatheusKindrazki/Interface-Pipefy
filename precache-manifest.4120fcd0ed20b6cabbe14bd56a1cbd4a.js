3e4c1759fc2dbf063062e9e90d54ffeabb29a225
Matheus Kindrazki <matheus@mentores.com.br>
2019-09-08 00:06:15 -0300

    Updates

---
+++ precache-manifest.4120fcd0ed20b6cabbe14bd56a1cbd4a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641be767cdfab71c132e1dc5fda2f7e8",
+    "url": "/Interface-Pipefy/index.html"
+  },
+  {
+    "revision": "deb85ea61bb3777d05ca",
+    "url": "/Interface-Pipefy/static/js/2.73b5f73d.chunk.js"
+  },
+  {
+    "revision": "e251d1764dcf32139fc1",
+    "url": "/Interface-Pipefy/static/js/main.5d90072d.chunk.js"
+  },
+  {
+    "revision": "091ecef95c2beff279ed",
+    "url": "/Interface-Pipefy/static/js/runtime~main.3debb867.js"
+  }
+]);
